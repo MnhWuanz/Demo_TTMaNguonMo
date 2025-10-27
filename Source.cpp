727026d0afdf8cc4fb2b6e9088d1f3100bb17717
NguyenManhQuan <mnhquan0109@gmail.com>
2025-10-27 08:10:57 +0700

    USER A

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	return a+b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH  THUC HANH GIT";
-
+	cout<<"Nhap a:";
+	cin>>a;
+	cout << "Nhap b:";
+	cin>>b;
+	cout<<"USER A";
+	cout<<"Tong: " <<tong(a,b) <<endl;
 	system("pause");
 	return 0;
 }
