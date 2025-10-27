dd9ea3f4626eb737c8a50076cfb739f54065f7c7
NguyenManhQuan <mnhquan0109@gmail.com>
2025-10-27 08:55:31 +0700

    Last Change

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
 using namespace std;
-<<<<<<< HEAD
-int tong(int a, int b);
-int hieu(int a, int b){
-	return a-b;
-}
-=======
+
+
 int tong(int a, int b){
 	return a+b;
 }
-int hieu(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+}
 
 int tich(int a, int b);
   return a*b;
